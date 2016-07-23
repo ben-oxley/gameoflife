d4a76caac9f7a435e854f83c18448791d03a80f6
Ben Oxley <ben.oxley@gmail.com>
2016-07-23 20:41:26 +0100

    Eclipse project set for ChibiOS

---
+++ firmware/gol/.dep/crt1.o.d
@@ -0,0 +1,49 @@
+build/obj/crt1.o: ../ChibiOS/os/common/ports/ARMCMx/compilers/GCC/crt1.c \
+ /usr/local/gcc-arm-none-eabi-5_4-2016q2/lib/gcc/arm-none-eabi/5.4.1/include/stdbool.h \
+ ../ChibiOS/os/common/ports/ARMCMx/devices/STM32F0xx/cmparams.h \
+ ../ChibiOS/os/hal/boards/ST_STM32F0_DISCOVERY/board.h \
+ ../ChibiOS/os/ext/CMSIS/ST/STM32F0xx/stm32f0xx.h \
+ ../ChibiOS/os/ext/CMSIS/ST/STM32F0xx/stm32f051x8.h \
+ ../ChibiOS/os/ext/CMSIS/include/core_cm0.h \
+ /usr/local/gcc-arm-none-eabi-5_4-2016q2/lib/gcc/arm-none-eabi/5.4.1/include/stdint.h \
+ /usr/local/gcc-arm-none-eabi-5_4-2016q2/arm-none-eabi/include/stdint.h \
+ /usr/local/gcc-arm-none-eabi-5_4-2016q2/arm-none-eabi/include/machine/_default_types.h \
+ /usr/local/gcc-arm-none-eabi-5_4-2016q2/arm-none-eabi/include/sys/features.h \
+ /usr/local/gcc-arm-none-eabi-5_4-2016q2/arm-none-eabi/include/_newlib_version.h \
+ /usr/local/gcc-arm-none-eabi-5_4-2016q2/arm-none-eabi/include/sys/_intsup.h \
+ /usr/local/gcc-arm-none-eabi-5_4-2016q2/arm-none-eabi/include/sys/_stdint.h \
+ ../ChibiOS/os/ext/CMSIS/include/core_cmInstr.h \
+ ../ChibiOS/os/ext/CMSIS/include/core_cmFunc.h \
+ ../ChibiOS/os/ext/CMSIS/ST/STM32F0xx/system_stm32f0xx.h
+
+/usr/local/gcc-arm-none-eabi-5_4-2016q2/lib/gcc/arm-none-eabi/5.4.1/include/stdbool.h:
+
+../ChibiOS/os/common/ports/ARMCMx/devices/STM32F0xx/cmparams.h:
+
+../ChibiOS/os/hal/boards/ST_STM32F0_DISCOVERY/board.h:
+
+../ChibiOS/os/ext/CMSIS/ST/STM32F0xx/stm32f0xx.h:
+
+../ChibiOS/os/ext/CMSIS/ST/STM32F0xx/stm32f051x8.h:
+
+../ChibiOS/os/ext/CMSIS/include/core_cm0.h:
+
+/usr/local/gcc-arm-none-eabi-5_4-2016q2/lib/gcc/arm-none-eabi/5.4.1/include/stdint.h:
+
+/usr/local/gcc-arm-none-eabi-5_4-2016q2/arm-none-eabi/include/stdint.h:
+
+/usr/local/gcc-arm-none-eabi-5_4-2016q2/arm-none-eabi/include/machine/_default_types.h:
+
+/usr/local/gcc-arm-none-eabi-5_4-2016q2/arm-none-eabi/include/sys/features.h:
+
+/usr/local/gcc-arm-none-eabi-5_4-2016q2/arm-none-eabi/include/_newlib_version.h:
+
+/usr/local/gcc-arm-none-eabi-5_4-2016q2/arm-none-eabi/include/sys/_intsup.h:
+
+/usr/local/gcc-arm-none-eabi-5_4-2016q2/arm-none-eabi/include/sys/_stdint.h:
+
+../ChibiOS/os/ext/CMSIS/include/core_cmInstr.h:
+
+../ChibiOS/os/ext/CMSIS/include/core_cmFunc.h:
+
+../ChibiOS/os/ext/CMSIS/ST/STM32F0xx/system_stm32f0xx.h:
